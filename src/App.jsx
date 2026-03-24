77857366310cc9a8f764a76ee813c306ddd42d1f
Yousef Asaad <asaidyousef6@gmail.com>
2026-03-25 01:49:42 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { CartProvider } from "./context/CartContext";
+
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
+import Home from "./pages/Home";
+import Shop from "./pages/Shop";
+import SingleProduct from "./pages/SingleProduct";
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
+import Profile from "./pages/Profile";
+
+function App() {
+  return (
+    <CartProvider>
+      <BrowserRouter>
+        <Navbar /> {/* هيظهر في كل الصفحات */}
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/shop" element={<Shop />} />
+          <Route path="/product/:id" element={<SingleProduct />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+
+        <Footer /> {/* هيظهر في كل الصفحات */}
+      </BrowserRouter>
+    </CartProvider>
+  );
+}
+
+export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,21 @@
+import Navbar from '../components/Navbar'
+import HeroSection from '../components/HeroSection'
+import BestDeals from '../components/BestDeals'
+import ShopCategories from '../components/ShopCategories'
+import FeaturedProducts from '../components/FeaturedProducts'
+import FeaturedProducts2 from '../components/FeaturedProducts2'
+import MacbookBanner from '../components/MacbookBanner'
+import Newsletter from '../components/Newsletter'
+export default function Home() {
+  return (
+    <>
+      <HeroSection />
+      <BestDeals />
+      <ShopCategories />
+      <FeaturedProducts />
+      <FeaturedProducts2 />
+      <MacbookBanner />
+      <Newsletter />
+    </>
+  )
+}
