7eb29c34bed3edbf3e37e9e035e5846ccdf1d7a8
Yousef Asaad <asaidyousef6@gmail.com>
2026-03-25 12:22:03 +0200

    Fix merge conflict and add base path

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-<<<<<<< HEAD
-export default defineConfig({
-  plugins: [react()],
-  base: "/E_Commerce/",
-})
-=======
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/E-Commerce/",
 })
->>>>>>> 6ff25c739ae5fec161a55c178b63853d5c931cf9
